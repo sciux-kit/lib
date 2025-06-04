ad774373919973bfaf7c85d45f8c0fc6ef89bb1e
Acbox <acbox0328@gmail.com>
2025-06-04 14:58:47 +0800

    chore: move test package

--- packages/sciux/src/layout.ts
+++ packages/sciux/src/layout.ts
@@ -1,11 +1,12 @@
+import type { Component } from 'sciux-laplace'
 import type { RegisterContext } from './types'
 import { align, block, columns, flexbox, grid, rows } from '@sciux/layout'
 
 export default function ({ components }: RegisterContext): void {
   components.set('block', block)
-  components.set('flexbox', flexbox)
-  components.set('rows', rows)
-  components.set('columns', columns)
+  components.set('flexbox', flexbox as Component<string, any, any>)
+  components.set('rows', rows as Component<string, any, any>)
+  components.set('columns', columns as Component<string, any, any>)
   components.set('grid', grid)
   components.set('align', align)
 }
