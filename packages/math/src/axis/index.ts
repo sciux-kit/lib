ecc704927a4790bf2ec16d0a29cf79625ffc548d
Acbox <acbox0328@gmail.com>
2025-06-21 01:44:56 +0800

    feat: plane grid

--- packages/math/src/axis/index.ts
+++ packages/math/src/axis/index.ts
@@ -14,6 +14,8 @@ interface withLabelT {
   label: (count: number) => string
 }
 
+export const resolveDirection = (value: string): 1 | -1 => ['left', 'top'].includes(value) ? -1 : 1
+
 export const axis = defineComponent<'axis', typeof T.infer & withLabelT>((attrs) => {
   return {
     name: 'axis',
@@ -26,27 +28,26 @@ export const axis = defineComponent<'axis', typeof T.infer & withLabelT>((attrs)
       direction: 'right',
     },
     setup(children) {
-      const resolve = (value: string): 1 | -1 => ['left', 'top'].includes(value) ? -1 : 1
       const root = document.createElementNS('http://www.w3.org/2000/svg', 'g')
       root.setAttribute('transform', `translate(${attrs.x.value}, ${attrs.y.value})`)
       const axes = document.createElementNS('http://www.w3.org/2000/svg', 'g')
       // axis line
       const line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
-      line.setAttribute(['left', 'right'].includes(attrs.direction.value) ? 'x1' : 'y1', (attrs.range.value[0] * attrs.division.value * resolve(attrs.direction.value)).toString())
-      line.setAttribute(['left', 'right'].includes(attrs.direction.value) ? 'x2' : 'y2', (attrs.range.value[1] * attrs.division.value * resolve(attrs.direction.value)).toString())
+      line.setAttribute(['left', 'right'].includes(attrs.direction.value) ? 'x1' : 'y1', (attrs.range.value[0] * attrs.division.value * resolveDirection(attrs.direction.value)).toString())
+      line.setAttribute(['left', 'right'].includes(attrs.direction.value) ? 'x2' : 'y2', (attrs.range.value[1] * attrs.division.value * resolveDirection(attrs.direction.value)).toString())
       axes.append(line)
       // axis arrow
       const arrow = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
       arrow.setAttribute('points', '0,0 -7,5 10,0 -7,-5 0,0')
-      arrow.setAttribute('transform', `translate(${['left', 'right'].includes(attrs.direction.value) ? '' : '0,'} ${attrs.range.value[1] * attrs.division.value * resolve(attrs.direction.value)}${['top', 'bottom'].includes(attrs.direction.value) ? '' : ' ,0'})
+      arrow.setAttribute('transform', `translate(${['left', 'right'].includes(attrs.direction.value) ? '' : '0,'} ${attrs.range.value[1] * attrs.division.value * resolveDirection(attrs.direction.value)}${['top', 'bottom'].includes(attrs.direction.value) ? '' : ' ,0'})
         rotate(${attrs.direction.value === 'left' ? '180' : attrs.direction.value === 'top' ? '270' : attrs.direction.value === 'bottom' ? '90' : '0'})`)
       axes.append(arrow)
       // axis ticks
       const ticks = document.createElementNS('http://www.w3.org/2000/svg', 'g')
       for (let i = attrs.range.value[0]; i < attrs.range.value[1]; i += 1) {
         const tick = document.createElementNS('http://www.w3.org/2000/svg', 'line')
-        tick.setAttribute(['left', 'right'].includes(attrs.direction.value) ? 'x1' : 'y1', (i * attrs.division.value * resolve(attrs.direction.value)).toString())
-        tick.setAttribute(['left', 'right'].includes(attrs.direction.value) ? 'x2' : 'y2', (i * attrs.division.value * resolve(attrs.direction.value)).toString())
+        tick.setAttribute(['left', 'right'].includes(attrs.direction.value) ? 'x1' : 'y1', (i * attrs.division.value * resolveDirection(attrs.direction.value)).toString())
+        tick.setAttribute(['left', 'right'].includes(attrs.direction.value) ? 'x2' : 'y2', (i * attrs.division.value * resolveDirection(attrs.direction.value)).toString())
         tick.setAttribute(['left', 'right'].includes(attrs.direction.value) ? 'y1' : 'x1', '-2')
         tick.setAttribute(['left', 'right'].includes(attrs.direction.value) ? 'y2' : 'x2', '2')
         ticks.append(tick)
@@ -56,11 +57,12 @@ export const axis = defineComponent<'axis', typeof T.infer & withLabelT>((attrs)
       const labels = document.createElementNS('http://www.w3.org/2000/svg', 'g')
       labels.setAttribute('stroke', 'none')
       labels.setAttribute('text-anchor', 'middle')
-      labels.setAttribute('font-size', '12px')
+      labels.setAttribute('dominant-baseline', 'middle')
+      labels.setAttribute('font-size', theme.size('3xs'))
       labels.style.fontFamily = theme.font('math')
       for (let i = attrs.range.value[0]; i < attrs.range.value[1]; i += 1) {
         const label = document.createElementNS('http://www.w3.org/2000/svg', 'text')
-        label.setAttribute(['left', 'right'].includes(attrs.direction.value) ? 'x' : 'y', (i * attrs.division.value * resolve(attrs.direction.value)).toString())
+        label.setAttribute(['left', 'right'].includes(attrs.direction.value) ? 'x' : 'y', (i * attrs.division.value * resolveDirection(attrs.direction.value)).toString())
         label.setAttribute(['left', 'right'].includes(attrs.direction.value) ? 'y' : 'x', '16')
         label.textContent = attrs.label.value(i)
         labels.append(label)
--- packages/math/src/plane/index.ts
+++ packages/math/src/plane/index.ts
@@ -1,3 +1,4 @@
+import { theme } from '@sciux/utils-theme'
 import { type } from 'arktype'
 import { defineComponent, ref } from 'sciux-laplace'
 import { axis } from '../axis'
@@ -10,6 +11,8 @@ const T = type({
   range: type('number[]'),
   xLabel: type.unknown,
   yLabel: type.unknown,
+  xDirection: type.enumerated('left', 'right'),
+  yDirection: type.enumerated('top', 'bottom'),
 })
 interface withLabelT {
   xLabel: (count: number) => string
@@ -26,6 +29,8 @@ export const plane = defineComponent<'plane', typeof T.infer & withLabelT>((attr
       division: 20,
       xLabel: (count: number) => count.toString(),
       yLabel: (count: number) => count.toString(),
+      xDirection: 'right',
+      yDirection: 'top',
     },
     setup(children) {
       const root = document.createElementNS('http://www.w3.org/2000/svg', 'g')
@@ -36,8 +41,8 @@ export const plane = defineComponent<'plane', typeof T.infer & withLabelT>((attr
         y: attrs.y,
         division: attrs.division,
         range: attrs.domain,
-        label: attrs.xLabel,
-        direction: ref('right'),
+        label: ref((count: number) => count === 0 ? '' : attrs.xLabel.value(count)),
+        direction: attrs.xDirection,
       }, {})
       root.append(<Node> xAxis.setup?.(() => []))
       // y-axis
@@ -46,9 +51,48 @@ export const plane = defineComponent<'plane', typeof T.infer & withLabelT>((attr
         y: attrs.y,
         division: attrs.division,
         range: attrs.range,
-        label: attrs.yLabel,
-        direction: ref('top'),
+        label: ref((count: number) => count === 0 ? '' : attrs.yLabel.value(count)),
+        direction: attrs.yDirection,
       }, {})
+      const originLabel = attrs.xLabel ? attrs.xLabel.value(0) : attrs.yLabel ? attrs.yLabel.value(0) : ''
+      const origin = document.createElementNS('http://www.w3.org/2000/svg', 'text')
+      origin.setAttribute('x', (attrs.x.value + 10).toString())
+      origin.setAttribute('y', (attrs.y.value + 10).toString())
+      origin.style.fill = theme.pallete('primary')
+      origin.style.fontFamily = theme.font('math')
+      origin.style.fontSize = theme.size('3xs')
+      origin.style.textAnchor = 'middle'
+      origin.style.dominantBaseline = 'middle'
+      origin.textContent = originLabel
+
+      // Grid
+      const grid = document.createElementNS('http://www.w3.org/2000/svg', 'g')
+      grid.setAttribute('stroke', theme.pallete('primary'))
+      grid.setAttribute('stroke-width', '0.5')
+      grid.setAttribute('fill', 'none')
+      for (let i = attrs.domain.value[0]; i < attrs.domain.value[1]; i += 1) {
+        if (i === attrs.domain.value[0])
+          continue
+        const line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+        line.setAttribute('x1', (i * attrs.division.value).toString())
+        line.setAttribute('y1', (attrs.range.value[0] * attrs.division.value).toString())
+        line.setAttribute('x2', (i * attrs.division.value).toString())
+        line.setAttribute('y2', (attrs.range.value[1] * attrs.division.value).toString())
+        grid.append(line)
+      }
+      for (let i = attrs.range.value[0]; i < attrs.range.value[1]; i += 1) {
+        if (i === attrs.range.value[0])
+          continue
+        const line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+        line.setAttribute('x1', (attrs.domain.value[0] * attrs.division.value).toString())
+        line.setAttribute('y1', (i * attrs.division.value).toString())
+        line.setAttribute('x2', (attrs.domain.value[1] * attrs.division.value).toString())
+        line.setAttribute('y2', (i * attrs.division.value).toString())
+        grid.append(line)
+      }
+
+      // Root
+      root.append(origin, grid)
       root.append(<Node> yAxis.setup?.(() => []))
       root.append(...children())
 
